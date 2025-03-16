a7bf3a9aa4c9dec96279e640827ec21de8bb5cc3
Clarkson <clarksoniyigun@gmail.com>
2025-03-16 17:23:50 +0100

    login status code updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,7 +82,7 @@ export const login = async (req, res) => {
       });
 
     } else{
-      res.status(401).json({ message: "Invalid email or password" });
+      res.status(400).json({ message: "Invalid email or password" });
     }
   } catch (error) {
     console.log("Error in login controller", error.message);
